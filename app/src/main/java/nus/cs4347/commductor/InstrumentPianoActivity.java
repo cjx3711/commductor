0f0076b514866fcbd19a84be3cf44a2780f2a93a
Jia Xun <chaijiaxun@gmail.com>
2017-03-27 00:06:51 +0800

    Added piano stuff

--- app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
+++ app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
@@ -2,12 +2,24 @@ package nus.cs4347.commductor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import nus.cs4347.commductor.display.Piano;
 
 public class InstrumentPianoActivity extends AppCompatActivity {
-
+    private static final String TAG = "InstrumentPianoActivity";
+    Piano piano;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrument_piano);
+
+        piano = (Piano) findViewById(R.id.view_piano);
+        piano.setPianoKeyListener(new Piano.PianoKeyListener() {
+            @Override
+            public void keyPressed(int id, int action) {
+                Log.d(TAG, id + " " + action);
+            }
+        });
     }
 }

175e74632ea0d3b6a54100d641ed31048e2aecf0
Jia Xun <chaijiaxun@gmail.com>
2017-03-26 23:40:55 +0800

    Added piano view and activity

---
+++ app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
@@ -0,0 +1,13 @@
+package nus.cs4347.commductor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstrumentPianoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrument_piano);
+    }
+}
---
+++ app/src/main/java/nus/cs4347/commductor/display/Key.java
@@ -0,0 +1,8 @@
+package nus.cs4347.commductor.display;
+
+/**
+ * Created by chaij on 26/03/2017.
+ */
+
+public class Key {
+}

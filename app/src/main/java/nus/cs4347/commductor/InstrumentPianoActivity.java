a3ea5922c29d9b25d8afe33a0d8a893642549e55
glutSolidSphere <lee.w.zheng@gmail.com>
2017-03-31 14:08:30 +0800

    Add synth start and stop on piano key press.
    Destroy function currently NOT called. Will have to find a way to manage thread deletion and stuff.
    Do I end a thread when the phone blackscreens, and then re-start the thread when you open it back up?

--- app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
+++ app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import nus.cs4347.commductor.display.Piano;
+import nus.cs4347.commductor.synthesizer.SynthThreadManager;
 
 public class InstrumentPianoActivity extends AppCompatActivity {
     private static final String TAG = "InstrumentPianoActivity";
@@ -16,6 +17,8 @@ public class InstrumentPianoActivity extends AppCompatActivity {
     private SoundPool soundPool;
     private int soundID;
     boolean loaded = false;
+
+    private SynthThreadManager synthThreadManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +34,13 @@ public class InstrumentPianoActivity extends AppCompatActivity {
             soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
         }
 
+        synthThreadManager = new SynthThreadManager();
+        synthThreadManager.init();
+
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {loaded = true;
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
             }
         });
         soundID = soundPool.load(this, R.raw.fart, 1);
@@ -43,19 +50,29 @@ public class InstrumentPianoActivity extends AppCompatActivity {
             @Override
             public void keyPressed(int id, int action) {
                 if (action == 1) {
+                    synthThreadManager.stopNote (id);
                     return;
                 }
                 Log.d(TAG, id + " " + action);
                 if (loaded) {
+                    synthThreadManager.playNote(id);
                     AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
                     float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
                     float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
                     float volume = actualVolume / maxVolume;
 
-                    soundPool.play(soundID, volume, volume, 1, 0, 1f);
+                    //soundPool.play(soundID, volume, volume, 1, 0, 1f);
                 }
             }
         });
     }
+
+    /*
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        synthThreadManager.destroy();
+    }
+    */
 }

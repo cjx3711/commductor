5e2c1a7432e5abaf9e97a00cfe96edaa4cdbebdd
glutSolidSphere <lee.w.zheng@gmail.com>
2017-03-31 14:06:39 +0800

    Thread for synthesizing sound.
    Each thread runs its own audioTrack.
    
    For now, audiotrack is paused by simply not writing into it when a boolean is false.
    Probably need to test this if it causes problems, but so far it hasn't been problematic.
    
    Also, currently some stuff is hardcodey, I will make it less hardcodey, but I want to see how it runs for you guys first.

---
+++ app/src/main/java/nus/cs4347/commductor/synthesizer/SynthesizerThread.java
@@ -0,0 +1,99 @@
+package nus.cs4347.commductor.synthesizer;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.util.Log;
+
+import nus.cs4347.commductor.AppData;
+
+/**
+ * Created by glutSolidSphere on 29/3/2017.
+ */
+
+public class SynthesizerThread extends Thread {
+    private static final double TWOPI = 8.*Math.atan(1.);
+
+    public int numHarmonics = 5;
+    public double ratios[];
+
+    private AudioTrack audioTrack;
+
+    private int bufferSize;
+    private short samples[];
+
+    public int samplingRate = 8000;
+    public int amplitude = 10000;
+    public double fundamentalFrequency = 440.f;
+    private int wave = 0;
+
+    private boolean isRunning = true;
+    private boolean isSynthesizing = false;
+
+    public SynthesizerThread() {
+        //Set the buffer size
+        bufferSize = AudioTrack.getMinBufferSize (
+            samplingRate,
+            AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT
+        );
+        //Initialize audiotrack object
+        audioTrack = new AudioTrack (
+            AudioManager.STREAM_MUSIC,
+            samplingRate,
+            AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT,
+            bufferSize * 2,
+            AudioTrack.MODE_STREAM
+        );
+
+        samples = new short[bufferSize];
+
+        ratios = new double [] {
+            5.0/15.0, 4.0/15.0, 3.0/15.0, 2.0/15.0, 1.0/15.0
+        };
+
+        audioTrack.play();
+    }
+
+    public void setFundamentalFrequency (int pitch) {
+        fundamentalFrequency = (pitch == 0) ? 0.f : (440.f * (Math.pow (2.f, (pitch - 69) / 12.f)));
+    }
+
+    public void startSynthesizing() {
+        isSynthesizing = true;
+    }
+
+    public void stopSythnesizing() {
+        isSynthesizing = false;
+        audioTrack.flush();
+    }
+
+    public void run() {
+        //Set process priority
+        setPriority(Thread.MAX_PRIORITY);
+
+        //Synthesis loop
+        while (isRunning) {
+            while (isSynthesizing) {
+                generateNote();
+                audioTrack.write(samples, 0, bufferSize);
+            }
+        }
+    }
+
+    public void destroy() {
+        audioTrack.stop();
+        audioTrack.release();
+    }
+
+    private void generateNote () {
+        for (int i = 0; i < bufferSize; i++) {
+            samples[i] = 0;
+            for (int j = 1; j < numHarmonics + 1; j++) {
+                samples[i] += amplitude * ratios[j-1] * AppData.getInstance().getLUT().getValAt(wave, j * fundamentalFrequency);
+            }
+            wave += 1;
+            wave = wave % samplingRate;
+        }
+    }
+}

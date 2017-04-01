ccab5fba045d37764b0b980a482e7d0652c2d2ce
glutSolidSphere <lee.w.zheng@gmail.com>
2017-04-01 14:46:32 +0800

    Remove soundpool.

--- app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
+++ app/src/main/java/nus/cs4347/commductor/InstrumentPianoActivity.java
@@ -25,54 +25,32 @@ public class InstrumentPianoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrument_piano);
 
-        // Load the sound
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            soundPool = new SoundPool.Builder()
-                    .setMaxStreams(10)
-                    .build();
-        } else {
-            soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-        }
-
         synthThreadManager = new SynthThreadManager();
         synthThreadManager.init();
-
-        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-            @Override
-            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                loaded = true;
-            }
-        });
-        soundID = soundPool.load(this, R.raw.fart, 1);
 
         piano = (Piano) findViewById(R.id.view_piano);
         piano.setPianoKeyListener(new Piano.PianoKeyListener() {
             @Override
             public void keyPressed(int id, int action) {
+                Log.d(TAG, id + " " + action);
                 if (action == 1) {
                     synthThreadManager.stopNote (id);
                     return;
                 }
-                Log.d(TAG, id + " " + action);
-                if (loaded) {
-                    synthThreadManager.playNote(id);
-                    AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-                    float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-                    float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                synthThreadManager.playNote(id);
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
-                    float volume = actualVolume / maxVolume;
-
-                    //soundPool.play(soundID, volume, volume, 1, 0, 1f);
-                }
+                float volume = actualVolume / maxVolume;
             }
         });
     }
 
-    /*
     @Override
     public void onDestroy() {
         super.onDestroy();
         synthThreadManager.destroy();
+        synthThreadManager = null;
     }
-    */
 }

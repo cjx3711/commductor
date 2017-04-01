2550c47a517c05c2c3f4966a9b6abc2e2968189d
Haritha <haritha1211@gmail.com>
2017-04-01 15:34:22 +0800

    minim bandpass test

--- app/src/main/java/nus/cs4347/commductor/InstrumentTriangleActivity.java
+++ app/src/main/java/nus/cs4347/commductor/InstrumentTriangleActivity.java
@@ -14,6 +14,7 @@ import nus.cs4347.commductor.bluetooth.BTPacketCallback;
 import nus.cs4347.commductor.bluetooth.BTPacketHeader;
 import nus.cs4347.commductor.gestures.GesturesTapCallback;
 import nus.cs4347.commductor.gestures.GesturesProcessor;
+import nus.cs4347.commductor_minim.ddf.minim.effects.BandPass;
 
 import android.content.Context;
 import android.hardware.Sensor;
@@ -37,10 +38,13 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
 
+import static android.media.AudioTrack.WRITE_NON_BLOCKING;
+
 
 public class InstrumentTriangleActivity extends AppCompatActivity {
 
     Button playButton;
+    Button playButton2;
     TextView message;
 
     private SoundPool soundPool;
@@ -75,12 +79,23 @@ public class InstrumentTriangleActivity extends AppCompatActivity {
         soundID = soundPool.load(this, R.raw.fart, 1);
 
         playButton = (Button) findViewById(R.id.button_play_triangle);
+        playButton2 = (Button) findViewById(R.id.button_play_triangle2);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    spamSound();
+                    spamSound1();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        playButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    spamSound2();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -94,7 +109,7 @@ public class InstrumentTriangleActivity extends AppCompatActivity {
         GesturesTapCallback tapCallback = new GesturesTapCallback(){
             public void tapDetected(){
                 try {
-                    spamSound();
+                    spamSound1();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -130,20 +145,8 @@ public class InstrumentTriangleActivity extends AppCompatActivity {
 
 
 
-    public void spamSound() throws IOException {
+    public void spamSound1() throws IOException {
 
-        // Getting the user sound settings
-        // This will be changed later on, when controlling it
-        // I'm leaving it here for reference's sake
-//        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-//
-//        float actualVolume = (float) audioManager
-//                .getStreamVolume(AudioManager.STREAM_MUSIC);
-//
-//        float maxVolume = (float) audioManager
-//                .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-//
-//        float volume = actualVolume / maxVolume;
         int Fs = 44100;
 
         int header_size = 44;
@@ -191,33 +194,41 @@ public class InstrumentTriangleActivity extends AppCompatActivity {
         AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, Fs,
                 AudioFormat.CHANNEL_OUT_MONO,
                 AudioFormat.ENCODING_PCM_16BIT,
-                (dataSize/2)-1,
+                buffsize,
                 AudioTrack.MODE_STREAM);
 
         try {
-            sound = new byte[dataSize]; // 2 bytes per audio frame, 16 bits
+            BandPass bandpass = new BandPass(19000,2000,44100);
             audioTrack.play();
-            while ((i = is.read(sound)) != -1) {
-                short[] sample = new short[dataSize/2];
-                ByteBuffer bb = ByteBuffer.wrap(sound);
-                bb.order( ByteOrder.LITTLE_ENDIAN);
-                int j = 0;
-                while( bb.hasRemaining()) {
-                    short v = bb.getShort();
-                    sample[j++] = v;
-                }
+            sound = new byte[dataSize];
+            int count = 0;
+            while ((count = is.read(sound, 0, dataSize)) > -1) {
 
-                audioTrack.write(sample, 0, sample.length);
-                Log.e("byte size", Arrays.toString(sound));
-                Log.e("byte size", Arrays.toString(sample));
+                float[] audio = byteToFloat(sound);
+                Log.e("byte count", Arrays.toString(audio));
+
+
+                bandpass.process(audio);
+                short[] shordio = floatToShort(audio);
+
+                // recombine signals for playback
+                // Load the sound
+                Log.e("byte count", String.valueOf(count));
+                Log.e("byte count", Arrays.toString(shordio));
+                Log.e("byte count", String.valueOf(audio.length));
+                audioTrack.write(shordio, 0, shordio.length);
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                    audioTrack.write(audio, 0, audio.length, WRITE_NON_BLOCKING);
+//                }
             }
+            Log.e("test byte", "out of loop");
+
+
         } catch (IOException e) {
 
         }
         audioTrack.stop();
         audioTrack.release();
-
-
 
         // Is the sound loaded already?
 //        if (loaded) {
@@ -227,6 +238,159 @@ public class InstrumentTriangleActivity extends AppCompatActivity {
 //        }
     }
 
+    public void spamSound2() throws IOException {
 
+        int Fs = 44100;
+
+        int header_size = 44;
+
+        byte[] sound = null;
+        int i = 0;
+        InputStream is = getResources().openRawResource(R.raw.fartwav);
+
+        ByteBuffer buffer = ByteBuffer.allocate(header_size);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        is.read(buffer.array(), buffer.arrayOffset(), buffer.capacity());
+
+        buffer.rewind();
+        buffer.position(buffer.position() + 20);
+        int format = buffer.getShort();
+
+        int channels = buffer.getShort();
+
+        int sample_rate = buffer.getInt();
+
+        buffer.position(buffer.position() + 6);
+        int bits_per_sample = buffer.getShort();
+
+        int dataSize = 0;
+        while (buffer.getInt() != 0x61746164) { // "data" marker
+            int size = buffer.getInt();
+            is.skip(size);
+            Log.e("byte me size2", String.valueOf(size));
+
+            buffer.rewind();
+            is.read(buffer.array(), buffer.arrayOffset(), 8);
+            buffer.rewind();
+        }
+        dataSize = buffer.getInt();
+        Log.e("byte me2", String.valueOf(format));
+        Log.e("byte me2", String.valueOf(channels));
+        Log.e("byte me2", String.valueOf(sample_rate));
+        Log.e("byte me2", String.valueOf(bits_per_sample));
+        Log.e("byte me2", String.valueOf(dataSize));
+
+        int buffsize = AudioTrack.getMinBufferSize(Fs, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        Log.e("I dont byte2", String.valueOf(buffsize));
+
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, Fs,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                buffsize,
+                AudioTrack.MODE_STREAM);
+
+        try {
+            BandPass bandpass = new BandPass(19000,2000,44100);
+            audioTrack.play();
+            sound = new byte[dataSize];
+            int count = 0;
+            while ((count = is.read(sound, 0, dataSize)) > -1) {
+
+                float[] audio = byteToFloat(sound);
+                Log.e("byte count2", Arrays.toString(audio));
+
+
+//                bandpass.process(audio);
+                short[] shordio = floatToShort(audio);
+
+                // recombine signals for playback
+                // Load the sound
+                Log.e("byte count2", String.valueOf(count));
+                Log.e("byte count2", Arrays.toString(shordio));
+                Log.e("byte count2", String.valueOf(audio.length));
+                audioTrack.write(shordio, 0, shordio.length);
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                    audioTrack.write(audio, 0, audio.length, WRITE_NON_BLOCKING);
+//                }
+            }
+            Log.e("test byte2", "out of loop");
+
+
+        } catch (IOException e) {
+
+        }
+        audioTrack.stop();
+        audioTrack.release();
+
+        // Is the sound loaded already?
+//        if (loaded) {
+//            soundPool.play(soundID, volume, volume, 1, 0, 1f);
+//            Log.e("Test", "Played sound");
+//
+//        }
+    }
+
+    /**
+     * Convert byte[] raw audio to 16 bit int format.
+     * @param rawdata
+     */
+    private short[] byteToShort(byte[] rawdata) {
+        short[] sample = new short[rawdata.length/2];
+        ByteBuffer bb = ByteBuffer.wrap(rawdata);
+        bb.order( ByteOrder.LITTLE_ENDIAN);
+        int j = 0;
+        while( bb.hasRemaining()) {
+            short v = bb.getShort();
+            sample[j++] = v;
+        }
+        return sample;
+    }
+
+    private float[] byteToFloat(byte[] audio) {
+        return shortToFloat(byteToShort(audio));
+    }
+
+
+    /**
+     * Convert int[] audio to 32 bit float format.
+     * From [-32768,32768] to [-1,1]
+     * @param audio
+     */
+    private float[] shortToFloat(short[] audio) {
+//        Log.d("SHORTTOFLOAT","INSIDE SHORTTOFLOAT");
+        Log.e("short byte", Arrays.toString(audio));
+        float[] converted = new float[audio.length];
+
+        for (int i = 0; i < converted.length; i++) {
+            // [-32768,32768] -> [-1,1]
+            converted[i] = audio[i] / 32768f; /* default range for Android PCM audio buffers) */
+
+        }
+
+        return converted;
+    }
+
+    private short[] floatToShort(float[] buffer) {
+        short[] shorts = new short[buffer.length];
+        for (int i = 0; i < buffer.length; i++) {
+            shorts[i] = (short) (buffer[i] * 32768f);
+        }
+        return shorts;
+    }
+
+
+    // Getting the user sound settings
+    // This will be changed later on, when controlling it
+    // I'm leaving it here for reference's sake
+//        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+//
+//        float actualVolume = (float) audioManager
+//                .getStreamVolume(AudioManager.STREAM_MUSIC);
+//
+//        float maxVolume = (float) audioManager
+//                .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+//
+//        float volume = actualVolume / maxVolume;
 
 }
